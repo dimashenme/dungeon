cff6d08397d46280621b9109e61c7bcc4ba98139
Dmitry Sustretov <dmitry@laowai.fritz.box>
2026-01-29 20:49:40 +0100

    remove leftover imports from Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE Arrows #-}
-
-import Control.Arrow
-import Control.Monad
-import Prelude hiding ((.))
-
-import Data.Array 
-import Data.Array.ST
-
-import Graphics.Vty
-import Graphics.Vty.Picture
-import Data.Default (def)
-
 import Dungeon.Game
 
 main :: IO ()
